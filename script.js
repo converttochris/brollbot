ced43babaee38133a391d475aab672c06e99ed8d
converttochris <github@christopherbroll.com>
2016-06-28 15:43:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Chris? Just say HELLO to get started.')
+            return bot.say('Grabbing Chris' bot. Say BOT to get started.)
                 .then(() => 'speak');
         }
     },
